2beb3a82eb198c3ccf192daea2be1643c3c68cb3
Inna Kukla <0969125133@ukr.net>
2023-03-08 01:15:24 +0200

    Create Modal.jsx

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,35 @@
+import { createPortal } from 'react-dom';
+import { ModalOverlay, ModalView } from './Modal.styled';
+import PropTypes from 'prop-types';
+
+import { useEffect } from 'react';
+const modalRoot = document.querySelector('#modal-root');
+
+export function Modal({ largeImageURL, tags, onEscape }) {
+  useEffect(() => {
+    window.addEventListener('keydown', handleEscapeDown);
+    return () => {
+      window.removeEventListener('keydown', handleEscapeDown);
+    };
+  }, []);
+
+  const handleEscapeDown = e => {
+    if (e.code === 'Escape') {
+      onEscape();
+    }
+  };
+
+  return createPortal(
+    <ModalOverlay>
+      <ModalView>
+        <img src={largeImageURL} alt={tags} />
+      </ModalView>
+    </ModalOverlay>,
+    modalRoot
+  );
+}
+
+Modal.propTypes = {
+  tags: PropTypes.string.isRequired,
+  largeImageURL: PropTypes.string.isRequired,
+};
